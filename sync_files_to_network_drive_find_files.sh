01d3c412b7ecf5289271efff7c3c555b7b215310
BeckenrandschwimmerTim <97850235+BeckenrandschwimmerTim@users.noreply.github.com>
2024-04-03 10:24:17 +0200

    Delete sync_files_to_network_drive_find_files.sh

--- sync_files_to_network_drive_find_files.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Search for all files with a name:
-# Example: find $source_dir -type f -name 'backup_this_whole_directory' |sort |uniq
-# Example: find /sharedfolders/Medien/Serien -type f -name 'backup_this_whole_directory' |sort |uniq
-
-# Search for all files with a name and rename it:
-# Example: find $source_dir -type f -name 'Backup_to_HDD030.txt' -execdir mv {} Backup_to_HDDXXX \;
